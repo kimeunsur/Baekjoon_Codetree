77945c6e9c768c3cc1925fe394850ceb6c78be77
kimeunsur <124233661+kimeunsur@users.noreply.github.com>
2025-03-14 19:59:16 +0900

    Update 1018.java

--- 1018.java
+++ 1018.java
@@ -1,43 +1,39 @@
 import java.io.*;
 import java.util.*;
 public class Main {
-    static int n,m;
-    static char[][] board;
+    public static int m,n;
+    public static char[][] board;
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
-        
         String[] input=br.readLine().split("\\s+");
         n=Integer.parseInt(input[0]);
         m=Integer.parseInt(input[1]);
         board=new char[n][m];
         for (int i=0;i<n;i++) {
-                board[i]=br.readLine().toCharArray();
+            board[i]=br.readLine().toCharArray();
         }
-        bw.write(findMinimumSquare()+"\n");
-
+        int MinValue=Integer.MAX_VALUE;
+        for (int i=0;i<=n-8;i++) {
+            for (int j=0;j<=m-8;j++) {
+                int value=findMinSquare(i,j);
+                MinValue=Math.min(value, MinValue);
+            }
+        }
+        bw.write(MinValue+"\n");
         bw.flush();
         br.close();
         bw.close();
     }
-    private static int findMinimumSquare() {
-        int minValue=Integer.MAX_VALUE;
-        for (int i=0;i<=n-8;i++) {
-            for (int j=0;j<=m-8;j++) {
-                minValue=Math.min(minValue,countMinimumSquare(i,j));
-            }
-        }
-        return minValue;
-    }
-    private static int countMinimumSquare(int startX, int startY) {
+    public static int findMinSquare(int startX, int startY) {
         int case1=0;
         int case2=0;
         for (int i=0;i<8;i++) {
             for (int j=0;j<8;j++) {
-                char expected1=((i+j)%2==0? 'B':'W');
-                char expected2=((i+j)%2==0? 'W':'B');
-                if (board[startX+i][startY+j]!=expected1) case1++;
-                if (board[startX+i][startY+j]!=expected2) case2++;
+                char expected1=((i+j)%2==0) ? 'B':'W';
+                char expected2=((i+j)%2==0) ? 'W':'B';
+                if (expected1!=board[startX+i][startY+j]) case1++;
+                if (expected2!=board[startX+i][startY+j]) case2++;
             }
         }
         return Math.min(case1,case2);
